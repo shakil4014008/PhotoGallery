e1594310df1cae308eb5f7e20d4fb57730230c86
shakil4014008 <shakil4014008@gmail.com>
2018-12-12 20:40:40 -0500

    add student controller and display data using view bag in the view

---
+++ MVC2017/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using MVC2017.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC2017.Controllers
+{
+    public class StudentController : Controller
+    {
+        IList<Student> studentList = new List<Student>() {
+                    new Student(){ StudentID=1, StudentName="Steve", Age = 21 },
+                    new Student(){ StudentID=2, StudentName="Bill", Age = 25 },
+                    new Student(){ StudentID=3, StudentName="Ram", Age = 20 },
+                    new Student(){ StudentID=4, StudentName="Ron", Age = 31 },
+                    new Student(){ StudentID=5, StudentName="Rob", Age = 19 }
+                };
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.TotalStudentsCount = studentList.Count();
+            return View();
+        }
+    }
+}
---
+++ MVC2017/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2017.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
