2ec7cd786eb2659a171bb78c5eb0729b137d722b
shakil4014008 <shakil4014008@gmail.com>
2018-12-12 20:48:40 -0500

    set project name, added menu for studentinfo and connect the link

--- MVC2017/RequirementFile.js
+++ MVC2017/RequirementFile.js
@@ -1,1 +1,5 @@
 ﻿//1//create photo library, add photo, download photo, browse photo in MVC
+
+
+//task 1: add student controller and display count data in the view using viewbag
+//-- set project name, added menu for studentinfo and connect the link
